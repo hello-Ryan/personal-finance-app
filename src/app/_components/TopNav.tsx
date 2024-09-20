b5a24035ea04bc3ecd9410e3c7d1b1e95480c5d2
Ryan Leung <ryanleung1029@gmail.com>
2024-09-20 22:39:34 +1000

    top nav component scaff

---
+++ src/app/_components/TopNav.tsx
@@ -0,0 +1,7 @@
+export default function TopNav() {
+    return (
+        <nav className="flex w-full items-center justify-between text-xl font-semibold border-gray-50">
+            <div>Navbar</div>
+        </nav>
+    )
+}
