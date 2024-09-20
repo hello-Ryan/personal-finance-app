dca0b1fc39be0b2debfe6f7db7b93ba7af05af2c
Ryan Leung <ryanleung1029@gmail.com>
2024-09-20 21:47:36 +1000

    connect db and env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-export default function HomePage() {
+import { db } from "~/server/db";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main>
       <div>
-        Hello test
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
       </div>
     </main>
   );
