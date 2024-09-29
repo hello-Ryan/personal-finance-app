127e097b76cb5b4a33a84cf8a21eeba2d148e093
Ryan Leung <ryanleung1029@gmail.com>
2024-09-29 17:52:17 +1000

    databse schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,16 +1,14 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { sql } from "drizzle-orm";
 import {
-  index,
   integer,
   pgTableCreator,
   serial,
   timestamp,
+  uuid,
   varchar,
 } from "drizzle-orm/pg-core";
-
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
  * database instance for multiple projects.
@@ -20,25 +18,41 @@ import {
 export const createTable = pgTableCreator(
   (name) => `personal-finance-app_${name}`,
 );
-
-export const transactions = createTable(
-  "transaction",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
-    userId: varchar("userId", { length: 256 }).notNull(),
-    amount: integer("amount").notNull(),
-    description: varchar("desription", { length: 256 }),
-    date: timestamp("date", { withTimezone: false }),
-    category: varchar("category", { length: 256 }),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
+export const users = createTable("user", {
+  id: serial("id").primaryKey().notNull(),
+  name: varchar("name", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+});
+export const categories = createTable("category", {
+  id: serial("id").primaryKey().notNull(),
+  userId: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
+  title: varchar("title", { length: 256 }).notNull(),
+  description: varchar("description", { length: 256 }),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
+export const transactions = createTable("transaction", {
+  id: serial("id").primaryKey().notNull(),
+  userId: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
+  categoryId: integer("category_id").references(() => categories.id),
+  name: varchar("name", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+  amount: integer("amount").notNull(),
+  description: varchar("desription", { length: 256 }),
+  date: timestamp("date", { withTimezone: true }).defaultNow().notNull(),
+});
