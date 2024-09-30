3671e91b7b7aef5a0fb1129a4a9424a4fc75750c
Ryan Leung <ryanleung1029@gmail.com>
2024-09-30 22:34:43 +1000

    hotfix db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,10 @@ import { db } from "~/server/db";
 import { TopNav } from "./_components";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
 
   return (
     <main>
       <TopNav/>
-      <div>
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-      </div>
-      
     </main>
   );
 }
