7c715bd2e5bec88980cc7888e59922aec23d421b
hleu0019 <ryanleung1029@gmail.com>
2025-02-23 15:40:16 +1100

    * Transaction table
    * Push mock data to db button
    
    * Auto refetch data upon pushing to db

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,69 @@
-export default function DashboardPage() {
-  return (
-    <div>Dashboard</div>
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import TransactionsTable, {
+  Transaction,
+} from "~/_components/TransactionsTable";
+
+import { db } from "~/server/db";
+import { transactions } from "~/server/db/schema";
+
+export default async function Page() {
+  const { userId } = auth();
+  type TransactionType = typeof transactions.$inferInsert;
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const transactionsFromDB = await db.query.transactions.findMany({
+    where: (users, { eq }) => eq(users.userId, userId),
+  });
+
+  const transactionsFormated = transactionsFromDB.map(
+    ({
+      id,
+      title,
+      description,
+      createdAt,
+      updatedAt,
+      transactionDate,
+      amount,
+    }) =>
+      ({
+        id,
+        title,
+        description,
+        createdAt,
+        updatedAt,
+        transactionDate,
+        amount,
+      }) as Transaction,
   );
-} 
+  async function pushDB() {
+    "use server";
+    await db.insert(transactions).values({
+      userId,
+      title: "Testing",
+      amount: Math.random() * 10,
+      description: "Testing description",
+    } as TransactionType);
+    revalidatePath('/')
+
+  }
+
+  function DBbutton() {
+    "use client";
+    return <button onClick={pushDB}>PUSH TO DB</button>;
+  }
+
+  return (
+    <div className="flex min-h-screen w-screen items-center justify-center flex-col gap-2">
+      <DBbutton />
+      <div className="w-3/4">
+      <TransactionsTable transactions={transactionsFormated} />
+
+      </div>
+    </div>
+  );
+}
